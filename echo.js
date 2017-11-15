60a4ffe2531d2d8cd6903d28997ad62b447f7d38
Jhon <c2404276@trbvn.com>
2017-11-15 14:31:39 +0100

    0.0.6

--- echo.js
+++ echo.js
@@ -3,6 +3,13 @@
  let sayInput = document.querySelectorAll("input[type=text]");
  let say = document.getElementById('say');
  sayButton.addEventListener("click", show);
+ window.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {
+     show();
+    } else if (e.keyCode !== 13){
+     say.innerHTML = 'Naćiśnij Enter';
+    }
+}, false);
 
  function show() {
   let isSaid = sayInput[0].value
