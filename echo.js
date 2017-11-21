c0c898fdf3ad78d629b4ff42f709f8ae2f8c5124
Jhon <c2404276@trbvn.com>
2017-11-21 20:48:57 +0100

    0.0.7
    Aded databinding, and repaired grid lyaout

--- echo.js
+++ echo.js
@@ -2,18 +2,26 @@
  let sayButton = document.getElementById("but");
  let sayInput = document.querySelectorAll("input[type=text]");
  let say = document.getElementById('say');
+ let showNow = document.getElementById('imidiet');
  sayButton.addEventListener("click", show);
- window.addEventListener("keydown", function (e) {
-    if (e.keyCode === 13) {
-     show();
-    } else if (e.keyCode !== 13){
-     say.innerHTML = 'Naćiśnij Enter';
-    }
-}, false);
+ window.addEventListener("keydown", print);
 
+// Keypres event detect
+ function print(e) {
+  if (e.keyCode === 13) {
+   show();
+  } else if (e.keyCode !== 13) {
+   say.innerHTML = 'Press Enter';
+  }
+ };
+// Display text from input
  function show() {
   let isSaid = sayInput[0].value
-  say.innerHTML = "Napisałeś: " + isSaid;
+  say.innerHTML = "You said: " + isSaid;
  };
+// Data-binding, imidiet display text from input
+ sayInput[0].addEventListener('input', function(evt) {
+  showNow.innerHTML = this.value;
+ });
 
 })();
